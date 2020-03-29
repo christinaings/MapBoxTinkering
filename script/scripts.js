8042eba7ab424851b9ce73fe0813e395dd6720d1
Christina <ings.christina@gmail.com>
2020-03-29 17:21:25 +0100

    fixes

--- script/scripts.js
+++ script/scripts.js
@@ -1,9 +1,15 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("map").style.left = "250px";
+    document.getElementById('addHouse').style.visibility = 'visible';
+    document.getElementById("openclose").text = String.fromCharCode('&lt;');
+    document.getElementById("openclose").setAttribute('onclick','closeNav()'); 
   }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "50";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("sidenav").style.width = "50";
+    document.getElementById("map").style.left = "50px";
+    document.getElementById('addHouse').style.visibility = 'hidden';
+    document.getElementById("openclose").text = String.fromCharCode('&gt;');
+    document.getElementById("openclose").setAttribute('onclick','openNav()');
   }
